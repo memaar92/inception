1c77f63db1f3b5e33bfaf42fdcea93dff98a5afe
memaar92 <marius.messerschmied@gmail.com>
2024-05-21 15:08:41 +0200

    add shell script for mariadb

---
+++ srcs/requirements/mariadb/tools/init_mdb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SQL_STATEMENTS="
+    CREATE DATABASE IF NOT EXISTS fruitdb;
+    USE fruitdb;
+    CREATE TABLE IF NOT EXISTS fruits (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        quantity INT NOT NULL
+    );
+    INSERT INTO fruits (name, quantity) VALUES
+        ('Apple', 10),
+        ('Banana', 20),
+        ('Cherry', 30);
+    "
+
+/etc/init.d/mariadb start
+mariadb -e "$SQL_STATEMENTS"
+/etc/init.d/mariadb stop
+
+exec "$@"
