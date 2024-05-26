b95f58c41030eb020cd3c07024d9a47e002744f9
memaar92 <marius.messerschmied@gmail.com>
2024-05-26 16:46:50 +0200

    nginx config not applied & wordpress not working

---
+++ srcs/requirements/wordpress/tools/entrypoint-wp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -f ./wordpress/wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
+	#Download wordpress
+	wget https://wordpress.org/latest.tar.gz
+	tar -xzvf latest.tar.gz
+	rm -rf latest.tar.gz
+
+    #Import env variables in the config file
+	cd /var/www/html/wordpress
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	#sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	mv wp-config-sample.php wp-config.php
+fi
+
+exec "$@"
