64f5ee147abfe5a8a951c2d806246b8f11530321
memaar92 <marius.messerschmied@gmail.com>
2024-05-29 14:42:01 +0200

    mount nginx config to different folder

--- srcs/requirements/mariadb/tools/init_mdb.sh
+++ srcs/requirements/mariadb/tools/init_mdb.sh
@@ -6,22 +6,12 @@
 #grant all privileges to user on database
 #flush privileges
 
-SQL_STATEMENTS="
-    CREATE DATABASE IF NOT EXISTS fruitdb;
-    USE fruitdb;
-    CREATE TABLE IF NOT EXISTS fruits (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        quantity INT NOT NULL
-    );
-    INSERT INTO fruits (name, quantity) VALUES
-        ('Apple', 10),
-        ('Banana', 20),
-        ('Cherry', 30);
-    "
-
 /etc/init.d/mariadb start
-mariadb -e "$SQL_STATEMENTS"
+mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER''@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+#mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'$WPHOST.$WPHOST' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -e "FLUSH PRIVILEGES;"
 /etc/init.d/mariadb stop
 
 exec "$@"
