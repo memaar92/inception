b3ad82085783d173d3389df835c68468c99690af
mamesser <mamesser@student.42wolfsburg.de>
2024-05-30 22:20:25 +0200

    add second user to wordpress

--- srcs/requirements/wordpress/tools/entrypoint-wp.sh
+++ srcs/requirements/wordpress/tools/entrypoint-wp.sh
@@ -11,9 +11,9 @@ else
 	mv wp-cli.phar /usr/local/bin/wp
 
 	wp core download --path=/var/www/html --allow-root
-	wp core config --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
+	wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
 	wp core install --allow-root --url=$WORDPRESS_URL --title="Test" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=$WORDPRESS_ADMIN_EMAIL
-	wp user create test --allow-root --role=editor --user_pass=$WORDPRESS_DB_PASSWORD
+	wp user create bob bob@user2.com --role=author --porcelain --allow-root
 	wp theme install twentytwentyfour --activate --allow-root
 fi
 #chmod -R 777 /var/www/html
