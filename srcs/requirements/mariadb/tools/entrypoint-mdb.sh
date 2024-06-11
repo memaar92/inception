aa30b55beaa4785f7846d0a68cba6faa2cdf4105
memaar92 <marius.messerschmied@gmail.com>
2024-06-11 10:56:03 +0200

    clean up docker compose and switch to debian bullseye

--- srcs/requirements/mariadb/tools/entrypoint-mdb.sh
+++ srcs/requirements/mariadb/tools/entrypoint-mdb.sh
@@ -4,9 +4,9 @@ mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/run/mysqld
 chmod -R 777 /var/lib/mysql # to be able to delete the database later on for testing purposes
 /etc/init.d/mariadb start
-mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;"
-mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASS';"
-mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASS';"
+mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
 mariadb -e "FLUSH PRIVILEGES;"
 /etc/init.d/mariadb stop
 
--- srcs/requirements/wordpress/tools/entrypoint-wp.sh
+++ srcs/requirements/wordpress/tools/entrypoint-wp.sh
@@ -13,7 +13,7 @@ else
 	wp core download --path=/var/www/html --allow-root
 	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --allow-root
 	wp core install --url=$WP_URL --title="Kobold" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --allow-root
-	wp user create bobby bob@mcbob.com --role=author --porcelain --allow-root
+	wp user create bobby bob@mcbob.com --role=author --user_pass=$WP_USER_PASS --porcelain --allow-root
 	wp theme install twentytwentyfour --activate --allow-root
 fi
 chmod -R 777 /var/www/html
