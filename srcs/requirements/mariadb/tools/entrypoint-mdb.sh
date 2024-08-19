1ec390b4654478e28b040aa6a7a7e3c25d67f1d9
memaar92 <marius.messerschmied@gmail.com>
2024-08-19 19:36:53 +0200

    add healthcheck for mariadb and hotfix mariadb startup issue

--- srcs/requirements/mariadb/tools/entrypoint-mdb.sh
+++ srcs/requirements/mariadb/tools/entrypoint-mdb.sh
@@ -4,7 +4,11 @@ mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/run/mysqld
 chmod -R 777 /var/run/mysqld
 chmod -R 777 /var/lib/mysql # to be able to delete the database later on for testing purposes
+
+
 /etc/init.d/mariadb start
+sleep 5
+#mariadb -e "USE mysql;"
 #mariadb -e "FLUSH PRIVILEGES;"
 #mariadb -e "ALTER USER 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASS';"
 #mariadb -e "FLUSH PRIVILEGES;"
