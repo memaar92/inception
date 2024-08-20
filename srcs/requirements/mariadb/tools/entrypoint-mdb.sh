f42fdc235aa8bec0cad89a5b8d12e0c0b2980c9f
memaar92 <marius.messerschmied@gmail.com>
2024-08-20 18:20:43 +0200

    fix pid 1 issue

--- srcs/requirements/mariadb/tools/entrypoint-mdb.sh
+++ srcs/requirements/mariadb/tools/entrypoint-mdb.sh
@@ -2,20 +2,19 @@
 
 mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/run/mysqld
-chmod -R 777 /var/run/mysqld
+# chmod -R 777 /var/run/mysqld
 chmod -R 777 /var/lib/mysql # to be able to delete the database later on for testing purposes
 
+mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
-/etc/init.d/mariadb start
-sleep 5
-#mariadb -e "USE mysql;"
-#mariadb -e "FLUSH PRIVILEGES;"
-#mariadb -e "ALTER USER 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASS';"
-#mariadb -e "FLUSH PRIVILEGES;"
-mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-mariadb -e "FLUSH PRIVILEGES;"
-/etc/init.d/mariadb stop
+echo "FLUSH PRIVILEGES;" >> init.sql
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" >> init.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';" >> init.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';" >> init.sql
+echo "FLUSH PRIVILEGES;" >> init.sql
+
+mariadbd --user=mysql --bootstrap < init.sql
+
+rm init.sql
 
 exec "$@"
